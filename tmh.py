49800f9f9486036dfa69593c3f4b89cf8288e6cd
Seth VanHeulen <svanheulen@gmail.com>
2013-10-25 12:07:38 -0400

    added support for DXT1 format and refactor deblocking code

--- tmh.py
+++ tmh.py
@@ -18,10 +18,28 @@
 import argparse
 import array
 import struct
+
 from PIL import Image
 
 
-def decode_palette(mode, data):
+def deblock(mode, width, data):
+    bw = (8, 8, 8, 4, 32, 16, 8, 4, 4, 4, 4)[mode]
+    bh = (8, 8, 8, 8, 8, 8, 8, 8, 4, 4, 4)[mode]
+    data = array.array('I', data)
+    new = array.array('I')
+    for i in range(len(data)):
+        x = i % width
+        y = i // width
+        xb = x // bw
+        x %= bw
+        yb = y // bh
+        y %= bh
+        offset = bw*bh*xb + width*bh*yb + bw*y + x
+        new.append(data[offset])
+    return new.tobytes()
+
+
+def decode(mode, data):
     if mode == 0:
         data = array.array('H', data)
         new = bytearray()
@@ -48,10 +66,6 @@ def decode_palette(mode, data):
         return bytes(new)
     if mode == 3:
         return data
-    return None
-
-
-def decode_pixels(mode, data):
     if mode == 4:
         new = bytearray()
         for i in data:
@@ -60,6 +74,34 @@ def decode_pixels(mode, data):
         return bytes(new)
     if mode == 5:
         return data
+    if mode == 6:
+        return array.array('H', data)
+    if mode == 7:
+        return array.array('I', data)
+    if mode == 8:
+        new = bytearray()
+        for i in range(0, len(data), 8):
+            c = [decode(0, data[i+4:i+6])]
+            c.append(decode(0, data[i+6:i+8]))
+            temp = array.array('H', data[i+4:i+8])
+            if temp[0] > temp[1]:
+                c.append(bytes([(2*c[0][0]+c[1][0])//3, (2*c[0][1]+c[1][1])//3, (2*c[0][2]+c[1][2])//3, 255]))
+                c.append(bytes([(c[0][0]+2*c[1][0])//3, (c[0][1]+2*c[1][1])//3, (c[0][2]+2*c[1][2])//3, 255]))
+            else:
+                c.append(bytes([(c[0][0]+c[1][0])//2, (c[0][1]+c[1][1])//2, (c[0][2]+c[1][2])//2, 255]))
+                c.append(b'\x00\x00\x00\xff')
+            for d in data[i:i+4]:
+                new.extend(c[d & 3])
+                new.extend(c[d >> 2 & 3])
+                new.extend(c[d >> 4 & 3])
+                new.extend(c[d >> 6 & 3])
+        return bytes(new)
+    if mode == 9:
+        # TODO - DXT3 format
+        return None
+    if mode == 10:
+        # TODO - DXT5 format
+        return None
     return None
 
 
@@ -67,36 +109,30 @@ def extract_tmh(tmh_file):
     with open(tmh_file, 'rb') as tmh:
         tmh_header = struct.unpack('8s2I', tmh.read(16))
         if tmh_header[0] != b'.TMH0.14':
-            raise ValueError('Not a valid TMH file.')
+            print('Not a valid TMH file.')
+            return
         for i in range(tmh_header[1]):
             image_header = struct.unpack('4I', tmh.read(16))
             pixel_header = struct.unpack('3I2H', tmh.read(16))
-            image = Image.new('P', pixel_header[3:])
-            for t in range((pixel_header[0] - 16) // 128):
-                w = 32
-                if pixel_header[2] == 5:
-                    w = 16
-                tile = Image.frombytes('P', (w, 8), decode_pixels(pixel_header[2], tmh.read(128)))
-                x = t * w
-                y = (x // pixel_header[3]) * 8
-                x %= pixel_header[3]
-                image.paste(tile, (x, y))
-            if pixel_header[2] != 8:
-                palette_header = struct.unpack('4I', tmh.read(16))
-                palette_data = decode_palette(palette_header[2], tmh.read(palette_header[0] - 16))
-                if pixel_header[2] == 5:
-                    for p in range(palette_header[3] // 256):
-                        image.putpalette(palette_data[p*1024:p*1024+1024], 'RGBX')
-                        image.save('%s-i%02d-p%02d.png' % (tmh_file, i, p))
-                else:
-                    for p in range(palette_header[3] // 16):
-                        image.putpalette(palette_data[p*64:p*64+64], 'RGBX')
-                        image.save('%s-i%02d-p%02d.png' % (tmh_file, i, p))
+            pixel_data = decode(pixel_header[2], tmh.read(pixel_header[0] - 16))
+            if image_header[3] == 1:
+                clut_header = struct.unpack('4I', tmh.read(16))
+                clut_data = decode(clut_header[2], tmh.read(clut_header[0] - 16))
+                new = bytearray()
+                for p in pixel_data:
+                    new.extend(clut_data[p*4:p*4+4])
+                pixel_data = bytes(new)
+            pixel_data = deblock(pixel_header[2], pixel_header[3], pixel_data)
+            image_format = 'RGBA'
+            if pixel_header[2] > 7:
+                image_format = 'BGRA'
+            image = Image.frombytes('RGBA', pixel_header[3:], pixel_data, 'raw', image_format)
+            image.save('%s-%04d.png' % (tmh_file, i))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extracts all images from a TMH image pack file from Monster Hunter')
-    parser.add_argument('tmhfile', help='TMH file to extract')
+    parser = argparse.ArgumentParser(description='Extract all textures from a TMH texture package file')
+    parser.add_argument('inputfile', help='TMH input file')
     args = parser.parse_args()
-    extract_tmh(args.tmhfile)
+    extract_tmh(args.inputfile)
 
