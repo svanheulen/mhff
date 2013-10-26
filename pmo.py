ac1d8a9dacc91e7e8cf775f1dd770a7c24b35314
Seth VanHeulen <svanheulen@gmail.com>
2013-10-26 16:40:22 -0400

    rewrite pmo script for better compatability

--- pmo.py
+++ pmo.py
@@ -18,94 +18,152 @@
 import argparse
 import array
 import struct
-import sys
 
 
-def run_ge(commands):
-    vertex_format = None
-    index_format = None
-    transform = [1, 1, 1]
-    primatives = []
-    order = None
+def run_ge(commands, pmo, obj, offset):
+    vertex_address = None
+    index_address = None
+    primative = []
+    base_address = None
+    vertex_type = {}
+    offset_address = None
+    face_order = None
     for command in commands:
-        c = command >> 24;
-        if c == 0x04:
-            primatives.append([command & 0xffff, (command >> 16) & 7, order])
-        elif c == 0x12:
-            texture = (None, 'b', 'h', 'f')[command & 3]
-            color = (None, None, None, None, '2x', '2x', '2x', '4x')[(command >> 2) & 7]
-            normal = (None, 'b', 'h', 'f')[(command >> 5) & 3]
-            position = (None, 'b', 'h', 'f')[(command >> 7) & 3]
-            weight = (None, 1, 2, 4)[(command >> 9) & 3]
-            index_format = (None, 'B', 'H', None)[(command >> 11) & 3]
-            skinning = ((command >> 14) & 7) + 1
-            morphing = ((command >> 18) & 7) + 1
-            bypass = (command >> 23) & 1
-            vertex_format = ''
-            if weight is not None:
-                vertex_format += '%dx' % (skinning * weight)
-            if texture is not None:
-                vertex_format += '2%s' % texture
-                if bypass == 0:
-                    transform[0] = 1 << (struct.calcsize(texture) * 8 - 1)
-            if color is not None:
-                vertex_format += color
-            if normal is not None:
-                vertex_format += '3%s' % normal
-                if bypass == 0:
-                    transform[1] = (1 << (struct.calcsize(normal) * 8 - 1)) - 1
-            if position is not None:
-                vertex_format += '3%s' % position
-                if bypass == 0:
-                    transform[2] = (1 << (struct.calcsize(position) * 8 - 1)) - 1
-        elif c == 0x9b:
-            order = command & 1
-    return vertex_format, index_format, transform, primatives
+        command_type = command >> 24
+        if command_type == 0x01:
+            vertex_address = command & 0xffffff
+        elif command_type == 0x02:
+            index_address = command & 0xffffff
+        elif command_type == 0x04:
+            primative.append({'type': (command >> 16) & 7, 'count': command & 0xffff, 'order': face_order})
+        elif command_type == 0x10:
+            base_address == command & 0xffffff
+        elif command_type == 0x12:
+            vertex_type['texture'] = command & 3
+            vertex_type['color'] = (command >> 2) & 7
+            vertex_type['normal'] = (command >> 5) & 3
+            vertex_type['position'] = (command >> 7) & 3
+            vertex_type['weight'] = (command >> 9) & 3
+            vertex_type['index'] = (command >> 11) & 3
+            vertex_type['skinning'] = ((command >> 14) & 7) + 1
+            vertex_type['morphing'] = ((command >> 18) & 7) + 1
+            vertex_type['bypass'] = (command >> 23) & 1
+        elif command_type == 0x13:
+            offset_address = command & 0xffffff
+        elif command_type == 0x9b:
+            face_order = command & 1
+    vertex_format = ''
+    transform = [None, 1, 1, 1]
+    if vertex_type['weight'] != 0:
+        vertex_format += str(vertex_type['skinning']) + (None, 'B', 'H', 'f')[vertex_type['weight']]
+        transform[0] = (None, 0x80, 0x8000, 1)[vertex_type['weight']]
+    if vertex_type['texture'] != 0:
+        vertex_format += '2' + (None, 'B', 'H', 'f')[vertex_type['texture']]
+        if vertex_type['bypass'] == 0:
+            transform[1] = (None, 0x80, 0x8000, 1)[vertex_type['texture']]
+    if vertex_type['color'] != 0:
+        vertex_format += (None, None, None, None, 'H', 'H', 'H', 'I')[vertex_type['color']]
+    if vertex_type['normal'] != 0:
+        vertex_format += '3' + (None, 'b', 'h', 'f')[vertex_type['normal']]
+        if vertex_type['bypass'] == 0:
+            transform[2] = (None, 0x7f, 0x7fff, 1)[vertex_type['normal']]
+    if vertex_type['position'] != 0:
+        vertex_format += '3' + (None, 'b', 'h', 'f')[vertex_type['position']]
+        if vertex_type['bypass'] == 0:
+            transform[3] = (None, 0x7f, 0x7fff, 1)[vertex_type['position']]
+    vertex_size = struct.calcsize(vertex_format)
+    vertex_data = None
+    if index_address is not None:
+        vertex_data = pmo.read(index_address - vertex_address)
+        vertex_data = vertex_data[:(len(vertex_data) // vertex_size) * vertex_size]
+    else:
+        vertex_data_size = 0
+        for p in primative:
+            vertex_data_size += p['count'] * vertex_size
+        vertex_data = pmo.read(vertex_data_size)
+    v_offset = 0
+    vn_offset = 0
+    vt_offset = 0
+    for i in range(0, len(vertex_data), vertex_size):
+        vertex = list(struct.unpack(vertex_format, vertex_data[i:i+vertex_size]))
+        if vertex_type['weight'] is not 0:
+            for w in range(vertex_type['skinning']):
+                unused = vertex.pop(0) / transform[0]
+        if vertex_type['texture'] is not 0:
+            u = vertex.pop(0) / transform[1]
+            v = vertex.pop(0) / transform[1]
+            vt_offset += 1
+            obj.write('vt %f %f\n' % (u, v))
+        if vertex_type['color'] != 0:
+            unused = vertex.pop(0)
+        if vertex_type['normal'] != 0:
+            i = vertex.pop(0) / transform[2]
+            j = vertex.pop(0) / transform[2]
+            k = vertex.pop(0) / transform[2]
+            vn_offset += 1
+            obj.write('vn %f %f %f\n' % (i, j, k))
+        if vertex_type['position'] != 0:
+            x = vertex.pop(0) / transform[3]
+            y = vertex.pop(0) / transform[3]
+            z = vertex.pop(0) / transform[3]
+            v_offset += 1
+            obj.write('v %f %f %f\n' % (x, y, z))
+    for p in primative:
+        index = range(p['count'])
+        if vertex_type['index'] != 0:
+            index = array.array((None, 'B', 'H', 'I')[vertex_type['index']])
+            index.fromfile(pmo, p['count'])
+        r = range(p['count'] - 2)
+        if p['type'] == 3:
+            r = range(0, p['count'], 3)
+        for i in r:
+            one = ['', '', '']
+            two = ['', '', '']
+            three = ['', '', '']
+            if vertex_type['position'] != 0:
+                one[0] = index[i] + offset[0]
+                two[0] = index[i+1] + offset[0]
+                three[0] = index[i+2] + offset[0]
+            if vertex_type['texture'] != 0:
+                one[1] = index[i] + offset[1]
+                two[1] = index[i+1] + offset[1]
+                three[1] = index[i+2] + offset[1]
+            if vertex_type['normal'] != 0:
+                one[2] = index[i] + offset[2]
+                two[2] = index[i+1] + offset[2]
+                three[2] = index[i+2] + offset[2]
+            if p['order'] == 1:
+                obj.write('f %s/%s/%s %s/%s/%s %s/%s/%s\n' % tuple(one + two + three))
+            else:
+                obj.write('f %s/%s/%s %s/%s/%s %s/%s/%s\n' % tuple(two + one + three))
+    return [offset[0] + v_offset, offset[1] + vt_offset, offset[2] + vn_offset]
 
-def convert_pmo(pmo_file, out_file):
-    with open(pmo_file, 'rb') as pmo, open(out_file, 'w') as out:
-        header = struct.unpack('4s4sI4f2H2I4x3I8x', pmo.read(0x40))
-        pmo.seek(header[10])
-        section = []
-        for i in range((header[11] - header[10]) // 0x10):
-            section.append(struct.unpack('2BH3I', pmo.read(0x10)))
-        vertex_offset = 1
-        for s in section:
-            pmo.seek(header[13] + s[3])
-            vertex_format, index_format, transform, primatives = run_ge(array.array('I', pmo.read(s[4] - s[3])))
-            vertex_size = struct.calcsize(vertex_format)
-            vertex_count = (s[5] - s[4]) // vertex_size
-            if s[5] == 0:
-                vertex_count = 0
-                for p in primatives:
-                    vertex_count += p[0]
-            pmo.seek(header[13] + s[4])
-            for i in range(vertex_count):
-                vertex = struct.unpack(vertex_format, pmo.read(vertex_size))
-                out.write('vt %f %f\n' % (vertex[0] / transform[0], vertex[1] / transform[0]))
-                out.write('vn %f %f %f\n' % (vertex[2] / transform[1], vertex[3] / transform[1], vertex[4] / transform[1]))
-                out.write('v %f %f %f\n' % (vertex[5] / transform[2], vertex[6] / transform[2], vertex[7] / transform[2]))
-            pmo.seek(header[13] + s[5])
-            for p in primatives:
-                index = range(p[0])
-                if index_format is not None:
-                    index = array.array(index_format)
-                    index.fromfile(pmo, p[0])
-                index = [i + vertex_offset for i in index]
-                r = range(p[0]-2)
-                if p[1] == 3:
-                    r = range(0, p[0], 3)
-                for i in r:
-                    if p[2] == 1:
-                        out.write('f %d/%d/%d %d/%d/%d %d/%d/%d\n' % (index[i], index[i], index[i], index[i+1], index[i+1], index[i+1], index[i+2], index[i+2], index[i+2]))
-                    else:
-                        out.write('f %d/%d/%d %d/%d/%d %d/%d/%d\n' % (index[i+1], index[i+1], index[i+1], index[i], index[i], index[i], index[i+2], index[i+2], index[i+2]))
-            vertex_offset += vertex_count
+
+def convert_pmo(pmo_file, mtl_file, obj_file):
+    with open(pmo_file, 'rb') as pmo, open(obj_file, 'w') as obj:
+        header = struct.unpack('4s4sI4f2H8I', pmo.read(0x40))
+        offset = [1, 1, 1]
+        obj.write('mtllib %s\n' % mtl_file)
+        for i in range(header[7]):
+            pmo.seek(header[9] + i * 0x30)
+            object_header = struct.unpack('8f2I4H', pmo.read(0x30))
+            obj.write('g group%04d\n' % i)
+            for j in range(object_header[12]):
+                pmo.seek(header[10] + ((object_header[13] + j) * 0x10))
+                group_header = struct.unpack('2BH3I', pmo.read(0x10))
+                pmo.seek(header[13] + ((object_header[11] + group_header[0]) * 0x10))
+                material_header = struct.unpack('4I', pmo.read(0x10))
+                obj.write('usemtl texture%04d\n' % material_header[2])
+                pmo.seek(header[14] + group_header[3])
+                commands = array.array('I', pmo.read(group_header[4] - group_header[3]))
+                offset = run_ge(commands, pmo, obj, offset)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Converts a Monster Hunter pmo model file to Wavefront OBJ format')
     parser.add_argument('pmofile', help='pmo model file to convert')
-    parser.add_argument('outfile', help='OBJ output file')
+    parser.add_argument('mtlfile', help='MTL file to use')
+    parser.add_argument('objfile', help='OBJ output file')
     args = parser.parse_args()
-    convert_pmo(args.pmofile, args.outfile)
+    convert_pmo(args.pmofile, args.mtlfile, args.objfile)
 
